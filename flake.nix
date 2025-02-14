b3fec29a31cc28d6b574ca74028b1c12acdf7649
Nguyen Minh Tho <198738874+t0bynguyen@users.noreply.github.com>
2025-02-14 13:09:01 +0700

    Optimize vscode config

--- flake.nix
+++ flake.nix
@@ -21,9 +21,6 @@
 
     # Nixvim
     nixvim.url = "github:nix-community/nixvim/nixos-24.11";
-
-    # VSCode
-    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
   };
 
   outputs =
